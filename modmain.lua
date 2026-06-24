45129ac7b8e72f6625284b2d0137f8540511b035
BBGoat <bbgoat@vip.qq.com>
2026-06-24 20:09:52 +0800

    修改模组依赖

--- modmain.lua
+++ modmain.lua
@@ -5,10 +5,105 @@ GLOBAL.setmetatable(env, {
 })
 _G = GLOBAL
 
-if not rawget(_G, "Chinese_Pro") then return end -- 必须加载Chinese++ Pro模组才能运行
+-- 加载我的工具
+if not rawget(GLOBAL, "BBGOAT_utils") then
+    if TUNING.suggest_to_subscribe_bbgoat_basementmod then
+        return
+    end
+    TUNING.suggest_to_subscribe_bbgoat_basementmod = true
 
-Upvaluehelper = _G.Chinese_Pro.env.Upvaluehelper
-MOD_util = _G.Chinese_Pro.env.MOD_util
+	local function should_show_dig()
+		if TheNet:GetIsServer() and TheNet:GetServerIsDedicated() then
+			return false
+		end
+		if not TheFrontEnd then
+			return false
+		end
+		if IsMigrating() then
+			return false
+		end
+		return not InGamePlay()
+	end
+
+	local _languages = {
+		zh = true, --Chinese for Steam
+		zhr = true, --Chinese for WeGame
+		ch = true, --Chinese mod
+		chs = true, --Chinese mod
+		sc = true, --simple Chinese
+		chinese = true, --Chinese mod
+		zht = true, --traditional Chinese for Steam
+		tc = true, --traditional Chinese
+		cht = true, --Chinese mod
+	}
+	local lang = _G.LanguageTranslator and _G.LanguageTranslator.defaultlang
+	local isCH = lang and _languages[lang]
+
+	if not TheNet:IsDedicated() then
+		AddGamePostInit(function()
+			TheGlobalInstance:DoTaskInTime(0.1, function()
+				if not should_show_dig() then return end
+				local PopupDialogScreen = require "screens/redux/popupdialog"
+				TheFrontEnd:PushScreen(PopupDialogScreen(
+					modinfo.name,
+					isCH and "模组基础运行库缺失！\n你缺少了模组基础运行库，你必须去订阅才能继续使用本模组" or
+							"Mod Runtime Library Missing!\nYou are missing the required runtime library for this mod. Please subscribe to it before continuing to use this mod.",
+					{
+						{
+							text = isCH and "订阅/启用运行库模组" or "Subscribe/Enable mod",
+							cb = function()
+								local modname = "workshop-3750536829"
+								if table.contains(TheSim:GetModDirectoryNames(), modname) then
+									KnownModIndex:Enable(modname)
+									KnownModIndex:Save()
+									TheGlobalInstance:DoTaskInTime(0.5, function()
+										TheNet:Disconnect(true)
+										TheSim:ResetError()
+										StartNextInstance()
+									end)
+								else
+									VisitURL("https://steamcommunity.com/sharedfiles/filedetails/?id=3750536829")
+									TheSim:SubscribeToMod("workshop-3750536829")
+									TheFrontEnd:PopScreen()
+									TheFrontEnd:PushScreen(PopupDialogScreen(
+										isCH and "已订阅" or "Subscribed",
+										isCH and "请前往模组列表启用【冰冰羊的模组运行库】模组" or "Please go to the mod list to enable the runtime library mod named\n\"BBGOAT Utils\"",
+										{
+											{
+												text = isCH and "好的" or "OK",
+												cb = function()
+													TheFrontEnd:PopScreen()
+												end
+											}
+										}
+									))
+								end
+							end
+						},
+						{
+							text = isCH and "订阅运行库模组" or "Subscribe Mod Base",
+							cb = function()
+								VisitURL("https://steamcommunity.com/sharedfiles/filedetails/?id=3750536829")
+								TheSim:SubscribeToMod("workshop-3750536829")
+								TheFrontEnd:PopScreen()
+							end
+						},
+						{
+							text = isCH and "返回" or "Back",
+							cb = function()
+								TheFrontEnd:PopScreen()
+							end
+						},
+					}
+				))
+			end)
+		end)
+	end
+    return
+end
+
+Upvaluehelper = GLOBAL.BBGOAT_utils.Upvaluehelper
+MOD_util = GLOBAL.BBGOAT_utils.MOD_util
 
 modimport("ban_print.lua") -- 关掉萌萌的新print的垃圾消息(你是话痨吗)
 modimport("hook_getupvalue") -- 将萌萌的新模组里的getupvalue替换为我的版本
---
+++ modservercreationmain.lua
@@ -0,0 +1,10 @@
+local _G = GLOBAL
+local rawget = _G.rawget
+
+-- 尝试自动开启依赖模组
+if rawget(_G, "TheFrontEnd") then
+    local basementmod = "workshop-3750536829"
+    if not _G.KnownModIndex:IsModEnabledAny(basementmod) then
+        _G.KnownModIndex:Enable(basementmod)
+    end
+end
